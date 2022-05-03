1fbaab27bab92ea7afaaa0b5f3d64b60de9b92ac
Lucas Coutinho <lucas.coutinho@eagronom.com>
2022-05-03 16:33:31 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {Buffer} from 'buffer';
+import { create } from "ipfs-http-client"
+import {useState} from 'react'
 
 function App() {
+
+  const [file, setFile] = useState(null);
+  const [hash, setHash] = useState();
+  const ipfs = create('https://ipfs.infura.io:5001');
+
+
+  const retrieveFile = (e) => {
+    const data = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      console.log("Buffer data: ", Buffer(reader.result))
+      setFile(Buffer(reader.result));
+    }
+    e.preventDefault();  
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const post = ipfs.add(file)
+    .then(post => {
+      setHash(`https://ipfs.io/ipfs/${post.path}`)
+      console.log(post.path)
+    })
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form">
+        <input type="file" name="data" onChange={retrieveFile} />
+        <button type="submit" className="btn" onClick={handleSubmit}>Upload file</button>
+      </form>
+      <div className="display">
+        <img src={hash} alt=''></img>
+      </div>
     </div>
   );
 }
